358059dd884e71eea82804487c680906f4fa3cbb
Andrei Anoling <anoling.andrei@gmail.com>
2026-01-29 12:31:42 -0600

    Feat: Created controllers for getAllTickets and getTicketById

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -1,2 +1,18 @@
 import { Request, Response } from "express";
 import * as ticketService from "../servcies/ticketService";
+
+export const getAllTickets = async (req: Request, res: Response): Promise<void> => {
+    const tickets = ticketService.getAllTickets();
+    res.status(200).json({message: "All tickets retrieved successfully", data: tickets});
+}
+
+export const getTicketById = async (req: Request, res: Response): Promise<void> => {
+    const id = Number(req.params.id);
+    const ticket = ticketService.getTicketById(id);
+    
+    if (ticket) {
+        res.status(200).json({message: "Ticket retrieved successfully", data: ticket});
+    } else {
+        res.status(404).json({message: "Ticket not found"});
+    }
+}
