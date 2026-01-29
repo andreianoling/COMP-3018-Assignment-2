219e6cc3afb8ec5f2d62c11c8220d0b877e5ad34
Andrei Anoling <anoling.andrei@gmail.com>
2026-01-29 13:36:00 -0600

    Fix: Addressed syntax issues with createticket and deleteticket

--- src/api/v1/controllers/ticketController.ts
+++ src/api/v1/controllers/ticketController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from "express";
 import * as ticketService from "../services/ticketService";
 import { HTTP_STATUS } from "../../../../src/constants/httpConstants";
+import { Ticket } from "../services/ticketService";
 
 export const createTicket = async (req: Request, res: Response): Promise<void> => {
-    const newTicket: string = req.body;
-    ticketService.createTicket(newTicket);
+    const newTicket: Ticket = req.body;
+    await ticketService.createTicket(newTicket);
     res.status(HTTP_STATUS.CREATED).json({message: "Ticket created successfully", data: newTicket});
 }
 
@@ -24,8 +25,8 @@ export const getTicketById = async (req: Request, res: Response): Promise<void> 
     }
 }
 
-export const deleteTicket = (req: Request, res: Response): void => {
-    const {id} = req.params;
-    ticketService.deleteTicket(id);
+export const deleteTicket = async (req: Request, res: Response): Promise<void> => {
+    const id = Number(req.params.id);
+    await ticketService.deleteTicket(id);
     res.status(HTTP_STATUS.OK).json({message: `Ticket with id ${id} deleted successfully`});
 }
