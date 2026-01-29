0c14e668e517b5c2ccc3655092e1dc3c9fade7ab
Andrei Anoling <anoling.andrei@gmail.com>
2026-01-29 12:58:24 -0600

    Feat: Added route for updateticket

--- src/api/v1/routes/ticketRoutes.ts
+++ src/api/v1/routes/ticketRoutes.ts
@@ -1,5 +1,5 @@
 import express, {Router} from "express";
-import {createTicket, getAllTickets, getTicketById, deleteTicket} from "../controllers/ticketController";
+import {createTicket, getAllTickets, getTicketById, updateTicket, deleteTicket} from "../controllers/ticketController";
 
 const router: Router = express.Router();
 
@@ -8,6 +8,8 @@ router.post("/", createTicket);
 router.get("/", getAllTickets);
 
 router.get("/:id", getTicketById);
+
+router.put("/:id", updateTicket);
 
 router.delete("/:id", deleteTicket);
 
